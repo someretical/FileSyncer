2e6ac2b132f264774254301e65e07de788efc7af
someretical <29365738+someretical@users.noreply.github.com>
2023-05-03 14:52:14 +1000

    Add license headers

--- MainWindow.cpp
+++ MainWindow.cpp
@@ -1,8 +1,20 @@
-//
-// Created by someretical on 28/04/2023.
-//
-
-// You may need to build the project (run Qt uic code generator) to get "ui_MainWindow.h" resolved
+/*
+ * This file is part of FileSyncer
+ * Copyright (C) 2023 someretical
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <QDir>
 
